69be78b53d4e9e0ec9b9a503f5527d283d50a178
akhilk7 <89996405+akhilk7@users.noreply.github.com>
2024-06-17 08:11:54 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { useState } from "react";
 import './App.css';
 
 function App() {
-
-
+  
   const apiKey = "5282ce2488bdf23b5789c56215bbeb0d"
   const [inputCity, setInputCity] = useState("")
   const [data, setData] = useState({})
@@ -51,7 +50,7 @@ function App() {
         <div className="col-md-12 text-center mt-5">
 
           <div className="shadow rounded wetherResultBox">
-            <img className="weathorIcon" src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png" alt="https://w7.pngwing.com/pngs/619/382/png-transparent-404-error-error-404-panel-attention-work-oops-signalling-danger-protection-thumbnail.png" />
+            <img className="weathorIcon" src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png" alt="..." />
 
             <h5 className="weathorCity">
               {data?.name}
